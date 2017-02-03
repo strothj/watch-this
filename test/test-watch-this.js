5cc16cb22e68f6a0a85b15d158591e45392d1613
steve2482 <stevecb12684@yahoo.com>
2017-02-02 21:08:46 -0500

    test that server sent html file

--- test/test-watch-this.js
+++ test/test-watch-this.js
@@ -2,6 +2,17 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
 
-const {app} = require('../sever');
+const {app} = require('../server');
 
 chai.use(chaiHttp);
+
+describe('HTML', function() {
+  it('should return the html page and a 200 status code', function() {
+    return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.html;
+      });
+  });
+});
